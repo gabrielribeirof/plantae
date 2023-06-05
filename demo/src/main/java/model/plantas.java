4d6e62540b25545ed7f17878c96841d58eb1d49e
Willian Murayama <willianyos13@gmail.com>
2023-06-05 00:03:59 -0300

    Criação do formulário de plantas

---
+++ demo/src/main/java/model/plantas.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Willian
+ */
+public class plantas {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
