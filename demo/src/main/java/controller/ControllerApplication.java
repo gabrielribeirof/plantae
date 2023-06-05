88c3831aa1752db7860eaa759c2aae29c93a7f52
Willian Murayama <willianyos13@gmail.com>
2023-06-05 00:03:45 -0300

    Correção do caminho dos arquivos

--- demo/src/main/java/controller/ControllerApplication.java
+++ demo/src/main/java/controller/ControllerApplication.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.example.demo;
+package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -47,6 +47,11 @@ public class ControllerApplication {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/plantas")
+    public String plantas() {
+        return "plantas";
     }
 
     @PostMapping("/cadastro")
--- demo/src/main/java/controller/DemoApplication.java
+++ demo/src/main/java/controller/DemoApplication.java
@@ -1,4 +1,6 @@
-package com.example.demo;
+package controller;
+
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
