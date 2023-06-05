9d6a4248cc771b6a779f6faeb33088abb8e51e68
Willian Murayama <willianyos13@gmail.com>
2023-06-05 20:38:11 -0300

    Implementação de PreAuthorize

--- demo/src/main/java/com/example/demo/ControllerApplication.java
+++ demo/src/main/java/com/example/demo/ControllerApplication.java
@@ -6,6 +6,7 @@ package com.example.demo;
 
 //import model.Plantas;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +28,7 @@ public class ControllerApplication {
     UserRepository userRepository;
 
     @GetMapping("")
+    @PreAuthorize("isAuthenticated()")
     public String index() {
         return "index";
     }
