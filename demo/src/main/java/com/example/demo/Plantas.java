a195a1fa89fc6924c89e3e0f2343d15a3ce9a00a
Willian Murayama <willianyos13@gmail.com>
2023-06-05 11:18:46 -0300

    Corrreção do nome

--- demo/src/main/java/com/example/demo/Plantas.java
+++ demo/src/main/java/com/example/demo/Plantas.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package com.example.demo;
 
 /**
  *
  * @author Willian
  */
-public class plantas {
+public class Plantas {
 
     private String nome;
 
